890003177fea44ddae23cd10a4e1941c8270aaf0
Edward Payton <edwardjpayton@me.com>
2017-05-22 18:33:24 +0100

    Tidying up

--- rollup.config.js
+++ rollup.config.js
@@ -6,12 +6,6 @@ export default {
 	moduleName: 'ng.angularx-components',
 	globals: {
 		'@angular/core': 'ng.core',
-		'@angular/common': 'ng.common',
-		'rxjs/Observable': 'Rx',
-		'rxjs/ReplaySubject': 'Rx',
-		'rxjs/add/operator/map': 'Rx.Observable.prototype',
-		'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
-		'rxjs/add/observable/fromEvent': 'Rx.Observable',
-		'rxjs/add/observable/of': 'Rx.Observable'
+		'@angular/common': 'ng.common'
 	}
 }
